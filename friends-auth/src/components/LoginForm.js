97d2ee1897c07d6e0f1a6f6dc5a315ab737bba40
Leighton Fritze <Jay.Row.Fritze@gmail.com>
2019-12-17 13:08:35 -0500

    setup LoginForm.js and axiosWithAuth.js

--- friends-auth/src/components/LoginForm.js
+++ friends-auth/src/components/LoginForm.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-// import { axiosWithAuth } from "../utils/axiosWithAuth";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 import {
   Button,
   Form,
@@ -10,38 +10,72 @@ import {
   Segment
 } from "semantic-ui-react";
 
-const LoginForm = () => (
-  <Grid textAlign="center" style={{ height: "100vh" }} verticalAlign="middle">
-    <Grid.Column style={{ maxWidth: 450 }}>
-      <Header as="h2" color="teal" textAlign="center">
-        <Image src="shield.jpg" /> Log-in to your account
-      </Header>
-      <Form size="large">
-        <Segment stacked>
-          <Form.Input
-            fluid
-            icon="user"
-            iconPosition="left"
-            placeholder="Username"
-          />
-          <Form.Input
-            fluid
-            icon="lock"
-            iconPosition="left"
-            placeholder="Password"
-            type="password"
-          />
+export const LoginForm = props => {
+  console.log(props);
+  const [user, setUser] = useState({
+    credentials: {
+      username: "",
+      password: ""
+    }
+  });
+  const handleChange = event => {
+    setUser({
+      credentials: {
+        ...user.credentials,
+        [event.target.name]: event.target.value
+      }
+    });
+  };
+  const loginSubmit = event => {
+    event.preventDefault();
+    axiosWithAuth()
+      .post("/api/login", user.credentials)
+      .then(res => {
+        localStorage.setItem("token", res.data.payload);
+        user.props.history.push("/protected");
+      })
+      .catch(error =>
+        console.log("LoginForm.js axiosWithAuth error", error.res)
+      );
+  };
+  return (
+    <Grid textAlign="center" style={{ height: "100vh" }} verticalAlign="middle">
+      <Grid.Column style={{ maxWidth: 450 }}>
+        <Header as="h2" color="teal" textAlign="center">
+          <Image src="shield.jpg" /> Log-in to your account
+        </Header>
+        <Form size="large" onSubmit={loginSubmit}>
+          <Segment stacked>
+            <Form.Input
+              fluid
+              icon="user"
+              iconPosition="left"
+              placeholder="Username"
+            />
+            <Form.Input
+              fluid
+              icon="lock"
+              type="text"
+              name="username"
+              label="username"
+              value={props.username}
+              onCHange={handleChange}
+              iconPosition="left"
+              placeholder="Password"
+              type="password"
+            />
 
-          <Button color="teal" fluid size="large">
-            Login
-          </Button>
-        </Segment>
-      </Form>
-      <Message>
-        New to us? <a href="#">Sign Up</a>
-      </Message>
-    </Grid.Column>
-  </Grid>
-);
+            <Button color="teal" fluid size="large">
+              Login
+            </Button>
+          </Segment>
+        </Form>
+        <Message>
+          New to us? <a href="#">Sign Up</a>
+        </Message>
+      </Grid.Column>
+    </Grid>
+  );
+};
 
 export default LoginForm;
